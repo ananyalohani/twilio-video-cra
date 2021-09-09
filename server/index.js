19abc390452099b4aef90505341af60ad4099bb2
Ananya Lohani <61914211+ananyalohani@users.noreply.github.com>
2021-09-10 01:16:56 +0530

    feat: Add server code

---
+++ server/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`Server ready on http://localhost:${PORT}`);
+});
+
+function generateAccessToken(identity, room, config) {
+  // use the twilio helper library to get Access Tokens
+  const twilio = require('twilio');
+  const AccessToken = twilio.jwt.AccessToken;
+  const VideoGrant = AccessToken.VideoGrant;
+
+  let videoGrant;
+  if (room) videoGrant = new VideoGrant({ room });
+  else videoGrant = new VideoGrant();
+
+  const token = new AccessToken(
+    config.accountSid,
+    config.keySid,
+    config.secret
+  );
+  token.addGrant(videoGrant);
+  token.identity = identity;
+  return token;
+}
+
+// handle POST requests from the client to send
+// the generated Twilio Access Token
+app.get('/api/video-token/:identity/:room', (req, res) => {
+  // read the required environment variables from .env file
+  require('dotenv').config();
+  const config = {
+    accountSid: process.env.TWILIO_ACCOUNT_SID,
+    keySid: process.env.TWILIO_API_KEY,
+    secret: process.env.TWILIO_API_SECRET,
+  };
+
+  const token = generateAccessToken(
+    req.params.identity,
+    req.params.room,
+    config
+  );
+
+  res.set('Content-Type', 'application/json');
+  res.send(
+    JSON.stringify({
+      token: token.toJwt(),
+    })
+  );
+});
--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Details from './Details';
 
 function App() {
+  useEffect(async () => {
+    const identity = 'ananya';
+    const room = 'room-1';
+    const res = await fetch(`/api/video-token/${identity}/${room}`);
+    const data = await res.json();
+
+    console.log(data);
+  }, []);
+
   return <div className='App'></div>;
 }
 
